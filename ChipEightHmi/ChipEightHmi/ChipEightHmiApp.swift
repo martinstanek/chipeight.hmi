5e6759480acf860031581f32b2585b342edd473e
Martin Stanek <stanekmartin@bluewin.ch>
2025-12-12 10:11:14 +0100

    project

---
+++ ChipEightHmi/ChipEightHmi/ChipEightHmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipEightHmiApp.swift
+//  ChipEightHmi
+//
+//  Created by Martin Stanek on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChipEightHmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChipEightHmi/ChipEightHmi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChipEightHmi
+//
+//  Created by Martin Stanek on 12.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
