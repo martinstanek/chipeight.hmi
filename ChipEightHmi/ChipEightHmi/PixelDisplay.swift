ab3e3cfaf46a670b828513a88e626fedaec7a267
Martin Stanek <stanekmartin@bluewin.ch>
2025-12-12 19:55:20 +0100

    more sprites

--- ChipEightHmi/ChipEightHmi/PixelDisplay.swift
+++ ChipEightHmi/ChipEightHmi/PixelDisplay.swift
@@ -4,9 +4,9 @@ public import Combine
 
 public final class PixelDisplay: NSObject, ObservableObject
 {
-    @Published var pixels: [[Bool]] = Array(repeating: Array(repeating: false,count: DisplayConfig.width),count: DisplayConfig.height)
-    @Published var foregroundColor: Color = .red
-    @Published var backgroundColor: Color = .black
+    @Published public var pixels: [[Bool]] = Array(repeating: Array(repeating: false,count: DisplayConfig.width),count: DisplayConfig.height)
+    @Published public var foregroundColor: Color = .red
+    @Published public var backgroundColor: Color = .black
     
     private let pixelsLock = NSLock()
     
@@ -43,6 +43,29 @@ public final class PixelDisplay: NSObject, ObservableObject
         }
     }
     
+    public func drawSprite(x: Int, y: Int, sprites: [UInt8])
+    {
+        pixelsLock.lock()
+        
+        defer
+        {
+            pixelsLock.unlock()
+        }
+        
+        DispatchQueue.main.async
+        {
+            for (spriteIndex, sprite) in sprites.enumerated()
+            {
+                let pixels = self.byteToBools(byte: sprite)
+                
+                for (pixelIndex, pixel) in pixels.enumerated()
+                {
+                    self.pixels[y + spriteIndex][x + pixelIndex] = pixel
+                }
+            }
+        }
+    }
+    
     private func setAllDisplay(state: Bool)
     {
         pixelsLock.lock()
@@ -55,6 +78,14 @@ public final class PixelDisplay: NSObject, ObservableObject
         DispatchQueue.main.async
         {
             self.pixels = Array(repeating: Array(repeating: state, count: DisplayConfig.width), count: DisplayConfig.height)
+        }
+    }
+    
+    private func byteToBools(byte: UInt8) -> [Bool]
+    {
+        (0..<8).reversed().map
+        {
+            bit in (byte & (1 << bit)) != 0
         }
     }
 }
