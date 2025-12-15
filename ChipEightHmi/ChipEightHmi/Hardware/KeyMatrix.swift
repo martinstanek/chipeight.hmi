c213f8d463b67bdaec113998b8bdc565afec5e31
Martin Stanek <stanekmartin@bluewin.ch>
2025-12-15 23:31:18 +0100

    check

--- ChipEightHmi/ChipEightHmi/Hardware/KeyMatrix.swift
+++ ChipEightHmi/ChipEightHmi/Hardware/KeyMatrix.swift
@@ -4,19 +4,29 @@ public import Combine
 
 public final class KeyMatrix: NSObject, ObservableObject
 {
-    @Published public var keys: [Bool] = Array(repeating: false,count: 16)
+    @Published public var keys: [Bool] = Array(repeating: false, count: 16)
     @Published public var lastPressed: UInt8?
     
     public func keyDown(key: String)
     {
-        var index = Essentials.hexStringToBytes(string: key)[0]
+        let index = Essentials.hexStringToBytes(string: key)[0]
+        
+        if keys[Int(index)]
+        {
+            return
+        }
+        
+        keys[Int(index)] = true
+        lastPressed = index
         
         print("Key down: \(index)")
     }
     
     public func keyUp(key: String)
     {
-        var index = Essentials.hexStringToBytes(string: key)[0]
+        let index = Essentials.hexStringToBytes(string: key)[0]
+        
+        keys[Int(index)] = false
         
         print("Key up: \(index)")
     }
